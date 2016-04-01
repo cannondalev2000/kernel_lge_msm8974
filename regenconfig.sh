61da971677ea64421b8183d979d116b2f89c9853
Ethan <cannondalev2000@gmail.com>
2016-04-01 11:05:57 -0700

    add script to regen all defconfigs for me

---
+++ regenconfig.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+make arch=ARM broken_d800_defconfig
+sleep 5
+rm -rf arch/arm/configs/broken_d800_defconfig
+sleep 5
+mv .config arch/arm/configs/broken_d800_defconfig
+sleep 5
+make arch=ARM broken_d801_defconfig
+sleep 5
+rm -rf arch/arm/configs/broken_d801_defconfig
+sleep 5
+mv .config arch/arm/configs/broken_d801_defconfig
+sleep 5
+make arch=ARM broken_d802_defconfig
+sleep 5
+rm -rf arch/arm/configs/broken_d802_defconfig
+sleep 5
+mv .config arch/arm/configs/broken_d802_defconfig
+sleep 5
+make arch=ARM broken_ls980_defconfig
+sleep 5
+rm -rf arch/arm/configs/broken_ls980_defconfig
+sleep 5
+mv .config arch/arm/configs/broken_ls980_defconfig
+sleep 5
+make arch=ARM broken_vs980_defconfig
+sleep 5
+rm -rf arch/arm/configs/broken_vs980_defconfig
+sleep 5
+mv .config arch/arm/configs/broken_vs980_defconfig
+sleep 5
+echo "all configs regenerated"
+sleep 5
+echo "making mrproper"
+sleep 5
+make mrproper
+sleep 5
+"all commands compleated have a broken day"
+
+
+
